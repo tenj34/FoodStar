f8c71f5fa1230abaf376e6fb955a0355a044e242
Juan Tenezaca <tenj34@gmail.com>
2018-10-28 19:07:08 +0000

    Skeleton added, includes gems to start and file structure to start

---
+++ Gemfile
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+ source "https://rubygems.org"
+ # gem "rails"
+ gem 'sinatra'
+gem 'sqlite3'
+gem 'activerecord', :require => "active_record"
+gem 'rake'
+gem 'shotgun'
+gem 'pry'
+gem 'sinatra-activerecord'
+gem 'require_all'
---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,13 @@
+require './config/environment'
+class ApplicationController < Sinatra::Base
+
+  configure do
+    set :public_folder, 'public'
+    set :views, 'app/views'
+    enable :sessions
+    set :session_secret, "starboy"
+  end
+   get '/' do
+    "This is working"
+  end
+end
