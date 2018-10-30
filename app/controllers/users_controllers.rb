ca8c24bf0505fe5802a052baac3743a90632b866
Juan Tenezaca <tenj34@gmail.com>
2018-10-30 17:19:22 +0000

    link user creator controller with view

--- app/controllers/users_controllers.rb
+++ app/controllers/users_controllers.rb
@@ -1,22 +1,29 @@
+require 'rack-flash'
+
 class UsersController < ApplicationController
+  use Rack::Flash
+
   get '/signup' do
     erb :'users/create_user'
   end
 
   post '/signup' do
     user = User.new(params)
-    if user.save
-      session[:user_id] = user.id
+    user.save
+    session[:user_id] = user.id
 
-      flash[:message] = "Welcome #{user.username}"
+    flash[:message] = "Welcome #{user.username}"
 
-      redirect "/reviews"
-    end
-    
+    redirect "/reviews"
+
   end
 
   get '/login' do
     erb :'users/login'
+  end
+
+  get '/reviews' do
+    erb :'users/reviews'
   end
 
 end
