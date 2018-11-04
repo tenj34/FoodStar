4f6c2c5404dba42ef2b9ac546554f11df17b723f
Juan Tenezaca <tenj34@gmail.com>
2018-11-04 20:04:54 +0000

    add new schema for reviews

--- db/migrate/20181030164626_create_reviews.rb
+++ db/migrate/20181030164626_create_reviews.rb
@@ -1,4 +1,8 @@
 class CreateReviews < ActiveRecord::Migration[5.2]
   def change
+    create_table :reviews do |t|
+      t.string :review
+      t.integer :user_id
+    end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,11 @@
 
 ActiveRecord::Schema.define(version: 2018_10_30_164626) do
 
+  create_table "reviews", force: :cascade do |t|
+    t.string "review"
+    t.integer "user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "username"
     t.string "email"
