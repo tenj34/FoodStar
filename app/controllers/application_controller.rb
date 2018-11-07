6529f6f197bb17ff9560c37e331f3c99fc385637
Juan Tenezaca <tenj34@gmail.com>
2018-11-07 00:40:35 +0000

    add helper method for defining current user and logged in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,19 @@ class ApplicationController < Sinatra::Base
     enable :sessions
     set :session_secret, "starboy"
   end
+
    get '/' do
     erb :index
   end
+
+  helpers do
+    def logged_in?
+      !!current_user
+    end
+
+    def current_user
+      @current_user ||= User.find(session[:user_id]) if session [:user_id]
+    end
+  end
+
 end
