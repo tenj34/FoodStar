9afe1a42506b2a67fa4577b3e6f9fbf771e2a72c
Juan Tenezaca <tenj34@gmail.com>
2018-11-07 00:36:42 +0000

    connect submit button with appropriate controller logic

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -2,4 +2,13 @@ class ReviewsController < ApplicationController
   get '/new' do
     erb :'reviews/new'
   end
+
+  post '/reviews' do
+    if params["review"] !=""
+      @review = current_user.reviews.create(review: params[:review])
+      redirect '/reviews'
+    else
+      redirect '/new'
+    end
+  end
 end
