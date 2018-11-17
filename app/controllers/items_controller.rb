1c166f8db2cf0d5aadc1b1c38d26abb4ebe375d3
Juan Tenezaca <tenj34@gmail.com>
2018-11-17 18:40:14 +0000

    add get for reviews new

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -9,4 +9,13 @@ class ItemsController < ApplicationController
       redirect to '/login'
    end
   end
+
+  get '/reviews/new' do
+    if logged_in?
+      erb :'reviews/create_review'
+    else
+      redirect to '/login'
+    end
+  end
+
 end
